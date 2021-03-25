178138250259f7a860be51113b4e0c3e0cfae81e
Sam Wen <samw@ritani.com>
2021-03-25 09:05:12 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const { exec } = require('child_process');
+
+class LambdaEmfiles {
+
+    constructor() {
+        this.is_new = true;
+        this.emfiles_count = 0;
+    }
+
+    /**
+     * public
+     * 
+     * @param {*} max_emfiles_needed estimated max file descriptors are needed
+     * @param {*} exit_process to exit the running process if not ok
+     * @returns 
+     */
+    async start_verify(max_emfiles_needed = 100, exit_process = true) {
+        if (!await this.__update_lambda_emfiles_count()) {
+            console.log(`*** ${this.is_new ? 'new' : 'old'} process, NOT OK`);
+        } else {
+            console.log(`*** ${this.is_new ? 'new' : 'old'} process, emfiles count: ${this.emfiles_count}`);
+        }
+        this.is_new = false;
+        if (exit_process && (!this.is_ok || 1000 - this.emfiles_count < max_emfiles_needed)) {
+            process.exit(1);
+        } else {
+            return this.is_ok;
+        }
+    }
+
+    /**
+     * public
+     * *
+     * @param {*} max_emfiles_needed estimated max file descriptors are needed
+     * @param {*} exit_process to exit the running process if not ok
+     *
+     */
+    async final_check(max_emfiles_needed = 100, exit_process = true) {
+        const emfiles_count = this.emfiles_count;
+        if (await this.__update_lambda_emfiles_count()) {
+            if (this.emfiles_count > emfiles_count) {
+                console.log(`*** emfiles count: ${this.emfiles_count}, leaks: ${this.emfiles_count - emfiles_count}`);
+            } else {
+                console.log('*** no leak emfiles found');
+            }
+        } else {
+            console.log('*** process, NOT OK');
+        }
+        if (exit_process && (!this.is_ok || 1000 - this.emfiles_count < max_emfiles_needed)) {
+            process.exit(1);
+        }
+    }
+
+    /**
+     * private implementation
+     */
+    __update_lambda_emfiles_count() {
+        this.is_ok = true;
+        return new Promise(resolve => {
+            exec(`ls /proc/${process.pid}/fd`, (err, stdout, stderr) => {
+                if (err || stderr) {
+                    this.is_ok = false;
+                    resolve(this.is_ok);
+                } else {
+                    const parts = stdout.split('\n');
+                    this.emfiles_count = parts.length - 1;
+                    resolve(this.is_ok);
+                }
+            });
+        });
+    }
+}
+
+module.exports = new LambdaEmfiles();
---
+++ test/index.js
@@ -0,0 +1,38 @@
+
+'use strict';
+
+const fs = require('fs');
+const lambda_emfiles = require('@samwen/lambda-emfiles');
+
+// eslint-disable-next-line no-unused-vars
+exports.lambdaHandler = async (event, context) => {
+    console.log(event);
+    let max_fds_needed = 100;
+    if (event.max_fds_needed) {
+        max_fds_needed = event.max_fds_needed;
+    }
+    let leaks = 100;
+    if (event.leaks) {
+        leaks = event.leaks;
+    }
+    try {
+        await lambda_emfiles.start_verify(max_fds_needed);
+        //
+        // simulate file descriptor leaks
+        let i = 0;
+        for (; i < files; i++) {
+            const filename = '/tmp/test' + i + '.txt';
+            fs.open(filename, 'w', (err, fd) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await lambda_emfiles.final_check(max_fds_needed);
+    }
+    return 'OK';
+};
+
